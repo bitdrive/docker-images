083253ac628a377e776a9152c2dc1eb588e41401
Xu YongBin <xuybin@qq.com>
2018-12-29 10:54:08 +0800

    init

--- kong/alpine/docker-entrypoint.sh
+++ kong/alpine/docker-entrypoint.sh
@@ -2,13 +2,16 @@
 set -e
 
 export KONG_NGINX_DAEMON=off
-
 if [[ "$1" == "kong" ]]; then
   PREFIX=${KONG_PREFIX:=/usr/local/kong}
 
   if [[ "$2" == "docker-start" ]]; then
+    need_install_plugins=${KONG_PLUGINS:-}
     kong prepare -p "$PREFIX"
-
+    if [ -n "$need_install_plugins" ];then
+        echo "need_install_plugins is not empty: ${need_install_plugins}"
+        luarocks install kong-plugin-${need_install_plugins}
+    fi
     exec /usr/local/openresty/nginx/sbin/nginx \
       -p "$PREFIX" \
       -c nginx.conf

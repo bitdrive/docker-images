65b528b995eb428b9dbb14b3bf0e8e86937615fe
xuybin <xuybin@qq.com>
2019-01-02 15:02:05 +0800

    自动安装插件

--- kong/alpine/docker-entrypoint.sh
+++ kong/alpine/docker-entrypoint.sh
@@ -7,10 +7,19 @@ if [[ "$1" == "kong" ]]; then
 
   if [[ "$2" == "docker-start" ]]; then
     need_install_plugins=${KONG_PLUGINS:-}
+    # 测试 need_install_plugins="bundled, ,bundled,, hello ,myplugin,bundled"
+    need_install_plugins=${need_install_plugins//, /,}
+    need_install_plugins=${need_install_plugins// ,/,}
+    need_install_plugins=${need_install_plugins//,,/,}
+    need_install_plugins=${need_install_plugins//,bundled,/,}
+    need_install_plugins=${need_install_plugins//bundled,/}
+    need_install_plugins=${need_install_plugins//,bundled/}
+    
     kong prepare -p "$PREFIX"
     if [ -n "$need_install_plugins" ];then
-        echo "luarocks install kong-plugin-${need_install_plugins} --local"
-        luarocks install kong-plugin-${need_install_plugins} --local
+        need_install_plugins="luarocks install kong-plugin-"${need_install_plugins//,/ --local && luarocks install kong-plugin-}" --local"
+        echo "$need_install_plugins"
+        echo "${need_install_plugins}" | awk '{cmd=$0; system(cmd)}'
     fi
     exec /usr/local/openresty/nginx/sbin/nginx \
       -p "$PREFIX" \

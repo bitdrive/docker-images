80bc096400772bf496423836af8bbbf6e8ee43a1
xuybin <xuybin@qq.com>
2019-01-02 15:51:39 +0800

    自动安装指定

--- kong/alpine/docker-entrypoint.sh
+++ kong/alpine/docker-entrypoint.sh
@@ -10,14 +10,20 @@ if [[ "$1" == "kong" ]]; then
     # 测试 need_install_plugins="bundled, ,bundled,, hello ,myplugin,bundled"
     need_install_plugins=${need_install_plugins//, /,}
     need_install_plugins=${need_install_plugins// ,/,}
-    need_install_plugins=${need_install_plugins//,,/,}
     need_install_plugins=${need_install_plugins//,bundled,/,}
     need_install_plugins=${need_install_plugins//bundled,/}
     need_install_plugins=${need_install_plugins//,bundled/}
     
     kong prepare -p "$PREFIX"
-    if [ -n "$need_install_plugins" ];then
+    # 未设置KONG_LUA_PACKAGE_PATH时，才使用luarocks安装
+    if [[ -n "$need_install_plugins"  ]] && [[ -z "$KONG_LUA_PACKAGE_PATH"  ]];then
+      if [ -n "$KONG_PLUGINS_SERVER" ];then
+        # 设置KONG_PLUGINS_SERVER时，才指定server
+        need_install_plugins="luarocks install --server="$KONG_PLUGINS_SERVER" kong-plugin-"${need_install_plugins//,/ --local && luarocks install kong-plugin-}" --local"
+      else
+        # 否则从默认root Manifest，即--server=http://luarocks.org处下载
         need_install_plugins="luarocks install kong-plugin-"${need_install_plugins//,/ --local && luarocks install kong-plugin-}" --local"
+      fi
         echo "$need_install_plugins"
         echo "${need_install_plugins}" | awk '{cmd=$0; system(cmd)}'
     fi
